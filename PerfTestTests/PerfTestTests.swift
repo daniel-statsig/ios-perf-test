9390b9138fc7c0406be9d83d86b1b9e16840c55c
daniel-statsig <daniel@statsig.com>
2023-03-15 13:06:45 -0700

    rm ui test

--- PerfTestTests/PerfTestTests.swift
+++ PerfTestTests/PerfTestTests.swift
@@ -1,12 +1,14 @@
 import XCTest
 
-import Statsig
+@testable import Statsig
 
 final class PerfTestTests: XCTestCase {
+    let env = DeviceEnvironment()
 
     override func setUpWithError() throws {
         let expectation = XCTestExpectation(description: "Statsig Initialize")
-        Statsig.start(sdkKey: "client-aGDsuuCYcisVAtySPNj1NFNRtnvsIlxHFo6rtshBbyo") { err in
+        Statsig.start(sdkKey: "client-aGDsuuCYcisVAtySPNj1NFNRtnvsIlxHFo6rtshBbyo",
+                      user: StatsigUser(userID: env.deviceModel)) { err in
             if let err = err {
                 fatalError(err)
             }
@@ -17,15 +19,68 @@ final class PerfTestTests: XCTestCase {
         self.wait(for: [expectation], timeout: 5)
     }
 
+    override func tearDownWithError() throws {
+        let expectation = XCTestExpectation(description: "Statsig Shutdown")
+        Statsig.shutdown()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            expectation.fulfill()
+        }
+        self.wait(for: [expectation], timeout: 5)
+    }
+
     func testGatePerformance() throws {
+        let (best, worst, average, metadata) = profile {
+            _ = Statsig.checkGate("test_public")
+        }
+
+        Statsig.logEvent("check_gate_worst", value: worst, metadata: metadata)
+        Statsig.logEvent("check_gate_best", value: best, metadata: metadata)
+        Statsig.logEvent("check_gate_average", value: average, metadata: metadata)
+    }
+
+    func testConfigPerformance() throws {
+        let (best, worst, average, metadata) = profile {
+            let config = Statsig.getConfig("a_config")
+            _ = config.getValue(forKey: "foo", defaultValue: "err")
+        }
+
+        Statsig.logEvent("get_config_worst", value: worst, metadata: metadata)
+        Statsig.logEvent("get_config_best", value: best, metadata: metadata)
+        Statsig.logEvent("get_config_average", value: average, metadata: metadata)
+    }
+
+    func testExperimentPerformance() throws {
+        let (best, worst, average, metadata) = profile {
+            let experiment = Statsig.getExperiment("an_experiment")
+            _ = experiment.getValue(forKey: "a_param", defaultValue: "err")
+        }
+
+        Statsig.logEvent("get_experiment_worst", value: worst, metadata: metadata)
+        Statsig.logEvent("get_experiment_best", value: best, metadata: metadata)
+        Statsig.logEvent("get_experiment_average", value: average, metadata: metadata)
+    }
+
+    func testLayerPerformance() throws {
+        let (best, worst, average, metadata) = profile {
+            let layer = Statsig.getLayer("a_layer")
+            _ = layer.getValue(forKey: "a_param", defaultValue: "err")
+        }
+
+        Statsig.logEvent("get_layer_worst", value: worst, metadata: metadata)
+        Statsig.logEvent("get_layer_best", value: best, metadata: metadata)
+        Statsig.logEvent("get_layer_average", value: average, metadata: metadata)
+    }
+
+    private func profile(_ task: () -> Void) -> (Double, Double, Double, [String: String]) {
         let iterations = 1000
         var best = CGFLOAT_MAX
         var worst = 0.0
         var sum = 0.0
+        var samples: [Double] = []
 
         for _ in 0...iterations {
             let start = CFAbsoluteTimeGetCurrent()
-            _ = Statsig.checkGate("test_public")
+            task()
             let end = CFAbsoluteTimeGetCurrent()
 
             let duration = (end - start) * 1000.0
@@ -33,12 +88,16 @@ final class PerfTestTests: XCTestCase {
             worst = max(worst, duration)
             best = min(best, duration)
             sum += duration
+            samples.append(duration)
         }
 
         let average = sum / Double(iterations)
 
-        XCTAssertLessThan(worst, 1)
-        XCTAssertLessThan(best, 0.1)
-        XCTAssertLessThan(average, 0.5)
+        let meta: [String: String] = [
+            "iterations": "\(iterations)",
+            "samples": "\(samples.sorted().reversed()[0...10])"
+        ]
+
+        return (best, worst, average, meta)
     }
 }
